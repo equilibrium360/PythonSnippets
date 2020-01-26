0e1d24556daa0416e2e86784645d71d95deadf16
equation <sivasaiemail@gmail.com>
2020-01-26 00:39:50 -0500

    updates

--- Misc/cmd_line_params.py
+++ Misc/cmd_line_params.py
@@ -2,3 +2,6 @@ import sys
 
 print("Number of Arguments " + str(len(sys.argv)))
 print(" Arguments List " + str(sys.argv))
+print(" Arguments One" + str(sys.argv[0]))
+print(" Arguments Two" + str(sys.argv[1]))
+print(" Arguments Two length" + str(len(str(sys.argv[1]))))
