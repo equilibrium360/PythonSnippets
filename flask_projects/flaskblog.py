ffd631902381096cfe44ee16569e01eccd3b9247
equation <sivasaiemail@gmail.com>
2020-04-20 09:24:08 -0400

    up

--- flask_projects/flaskblog.py
+++ flask_projects/flaskblog.py
@@ -1,5 +1,22 @@
 from flask import Flask
+
 app = Flask(__name__)
+
+
+stores = [
+
+    {
+        'name': 'My Wonderful store',
+        'items': [
+            {
+                'name': 'item name',
+                'price': 15.6
+             }
+
+        ]
+    }
+
+]
 
 @app.route('/index')
 def hello_world():
@@ -11,8 +28,23 @@ def create_store():
     pass
 
 
+@app.route('/store/<string:name>', methods=['GET'])  # http://127.0.0.1/store/some_name
+def get_store(name):
+    pass
+
+
 @app.route('/store', methods=['GET'])
-def get_store():
+def get_stores():
+    pass
+
+
+@app.route('/store/string:name/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+
+@app.route('/store/string:name/item', methods=['GET'])
+def get_item_in_store(name):
     pass
 
 
